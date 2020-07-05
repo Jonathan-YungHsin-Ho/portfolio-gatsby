daa2cb479431f3e33a17382fa39f00acb4b92269
Jonathan Ho <johnnyho82@gmail.com>
2020-07-05 14:45:43 -0400

    Update Angular icon

--- src/content/skills.js
+++ src/content/skills.js
@@ -5,6 +5,7 @@ import {
   FaJsSquare,
   FaReact,
   FaVuejs,
+  FaAngular,
   FaLess,
   FaSass,
   FaGit,
@@ -33,7 +34,7 @@ export const skillsFrontEnd = [
       { svg: "redux", skill: "Redux" },
       { svg: "apollo", skill: "Apollo Client" },
       { icon: <FaVuejs />, skill: "Vue.js (Vue Router, Vuex)" },
-      { icon: "angular", skill: "Angular" },
+      { icon: <FaAngular />, skill: "Angular" },
     ],
   },
   {
